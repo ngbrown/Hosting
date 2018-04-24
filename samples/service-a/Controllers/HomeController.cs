336083627fbf9ffb8b8c2771dd4210fc58463d7c
Nathan Brown <nathan@nkbrown.us>
2018-04-23 20:43:30 -0700

    Make projects depend on each other and inter-operate some.

--- samples/service-a/Controllers/HomeController.cs
+++ samples/service-a/Controllers/HomeController.cs
@@ -2,22 +2,51 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using service_a.Models;
 
 namespace service_a.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> log;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            this.log = log;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
+            try
+            {
+                HttpClient client = new HttpClient();
+                var result = await client.GetAsync("http://localhost:50572/");
+                if (result.IsSuccessStatusCode)
+                {
+                    var responseString = await result.Content.ReadAsStringAsync();
+                    log.LogInformation("Received {message}", responseString);
+                    ViewData["Message"] = responseString;
+                }
+                else
+                {
+                    log.LogWarning("Request encountered error {reason}", result.ReasonPhrase);
+                    ViewData["Message"] = "some error happened";
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogWarning("Request encountered exception {exception}", ex);
+                ViewData["Message"] = "exception happened";
+            }
 
             return View();
         }
--- samples/service-b/Startup.cs
+++ samples/service-b/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,13 +26,15 @@ namespace service_b
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            var random = new Random();
 
             app.Run(async (context) =>
             {
+                Thread.Sleep(random.Next(200) + 100);
+                if (random.Next(10) == 0)
+                {
+                    throw new Exception("You won the exception lottery");
+                }
                 await context.Response.WriteAsync("Hello World!");
             });
         }
